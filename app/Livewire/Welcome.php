73f0549f78697027c0d815b1aa9eb2de3bc20aee
Bruno Costa <bhcosta90@gmail.com>
2024-12-01 15:50:42 -0300

    change layout from welcome

--- app/Livewire/Welcome.php
+++ app/Livewire/Welcome.php
@@ -5,8 +5,10 @@ declare(strict_types = 1);
 namespace App\Livewire;
 
 use Illuminate\Contracts\View\View;
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
+#[Layout('components.layouts.guest')]
 class Welcome extends Component
 {
     public function render(): View
