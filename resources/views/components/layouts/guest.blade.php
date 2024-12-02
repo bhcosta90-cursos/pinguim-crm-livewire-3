b3a0c88bfb5c23be3a6cec7cb6a177349181db14
Bruno Costa <bhcosta90@gmail.com>
2024-12-02 16:09:52 -0300

    verify if exist impersonate in the session

--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -10,7 +10,9 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
-        <livewire:admin.user.user-impersonate-stop />
+        @if(session()->has('impersonate'))
+            <livewire:admin.user.user-impersonate-stop />
+        @endif
         <div class="flex justify-end px-6 pt-2 pb-3 bg-gray-50 border-b-2 border-gray-300">
             <x-dev />
         </div>
