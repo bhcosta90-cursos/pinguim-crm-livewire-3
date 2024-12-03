fa40273080a587bf5ce435d38f9189b698157356
Bruno Costa <bhcosta90@gmail.com>
2024-12-03 16:04:20 -0300

    change language

--- resources/views/components/layouts/partials/profile-dropdown.blade.php
+++ resources/views/components/layouts/partials/profile-dropdown.blade.php
@@ -19,8 +19,7 @@
     <div x-cloak x-show="show"
          class="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none"
          role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-        <a href="{{ route('profile') }}" class="block px-3 py-1 text-sm/6 text-gray-900" role="menuitem" tabindex="-1" id="user-menu-item-0">Your
-            profile</a>
+        <a href="{{ route('profile') }}" class="block px-3 py-1 text-sm/6 text-gray-900" role="menuitem" tabindex="-1" id="user-menu-item-0">@lang('Your profile')</a>
         <form method="post" action="{{ route('logout') }}">
             @csrf
             <button class="block px-3 py-1 text-sm/6 text-gray-900" role="menuitem" tabindex="-1" id="user-menu-item-1">
