ed195deead5476ef92146c224af5c55c30e65fee
Bruno Costa <bhcosta90@gmail.com>
2024-12-03 13:28:46 -0300

    change modal to slide on the create customer

--- resources/views/livewire/admin/customer/customer-create.blade.php
+++ resources/views/livewire/admin/customer/customer-create.blade.php
@@ -1,11 +1,11 @@
 <div>
-    <x-button primary outline wire:click="$toggle('modal')" label="New customer"/>
+    <x-button primary outline wire:click="$toggle('slide')" label="New customer"/>
 
-    <x-modal title="New customer" form>
-        <div class="flex gap-3">
+    <x-slide title="New customer" form>
+        <div class="space-y-3">
             <x-input wire:model="customer.name" label="Name" />
             <x-input wire:model="customer.email" label="Email" />
             <x-input wire:model="customer.phone" label="Phone" />
         </div>
-    </x-modal>
+    </x-slide>
 </div>
